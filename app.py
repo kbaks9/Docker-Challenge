c846574611f82279921e725217baa5f765216750
kbaks9 <kamaal.bakar@outlook.com>
2025-12-13 00:48:39 +0000

    Moved html from the python class to a templates folder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import redis
 import os
 
@@ -9,15 +9,13 @@ r = redis.Redis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def hello_world():
-    return '''
-    <h1>Homepage of the CoderCo Challenge</h1>
-    <p>Click to visit other page: <a href="/count">here</a></p>
-    '''
+    return render_template('home.html')
 
 @app.route('/count')
 def count():
-    count = r.incr('visits')
-    return f'There has been {count} visitors'
+    visitor_count = r.incr('visits')  # define visitor_count variable
+    return render_template('count.html', count=visitor_count)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
